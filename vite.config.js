c2b407c2e1614baebdae73fec4b00a2cd8313be4
Egor Bondar <erop961515@gmail.com>
2026-09-20 05:43:07 +0300

    feat: gh
    Signed-off-by: Egor Bondar <erop961515@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/vdk/',
+  base: '',
   server: { host: true, allowedHosts: true },
   preview: { host: true, allowedHosts: true },
 });
