57869646fbf764ef7545265ff4cc64c5cbe376c0
Egor Bondar <erop961515@gmail.com>
2026-09-20 05:32:56 +0300

    fix github pages base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/vdk/',
   server: { host: true, allowedHosts: true },
   preview: { host: true, allowedHosts: true },
 });
